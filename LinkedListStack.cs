1c9daa1b09e1702fe97d2c19669dd818ab46a85a
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-06 18:06:34 +0530

    [sailesh] Stack Methods

---
+++ LinkedListStack.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+public class LinkedListStack
+    {   
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                 node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ",+value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data+" ");
+                temp = temp.next;
+            }
+        }
+        internal void peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            Console.WriteLine("{0} The top element of Stack"+this.top.data);
+        }
+        internal void pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Cannot perform Pop/Delete");
+            }
+            Console.WriteLine("Value Popped is "+this.top.data);
+            Node temp;
+            temp = this.top.next;
+            this.top = temp;
+        }
+        internal void IsEmpty() 
+        {
+            while (this.top != null)
+            {
+                peek();
+                pop();
+            }
+            
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,17 +6,24 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            LinkedList list = new LinkedList();
-            list.Add(56);
-          //list.Add(30);
-            list.Add(70);
-          //list.Display();
-            list.Insert(2, 30);
-            list.Display();
-            list.Remove_First_Element();
-            list.Remove_Last_Element();
-            list.Search_Input_Value(30);
+            //LinkedList list = new LinkedList();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+            //list.Display();
+            //list.Insert(2, 30);
+            //list.Display();
+            //list.Remove_First_Element();
+            //list.Remove_Last_Element();
+            //list.Search_Input_Value(30);
 
+            LinkedListStack stack = new LinkedListStack();
+            //uc1
+            stack.Push(70);
+            stack.Push(56);
+            stack.Push(30);
+            //uc2
+            stack.pop();
 
 
         }
